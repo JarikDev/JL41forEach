c4608f4e4950e3302915c3eb9e093336eec6ff1a
ivan4000 <sxln1@mail.ru>
2018-10-29 14:41:54 +0300

    JL41forEach first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int arr[]={1,2,3};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        for (int i:arr) {
+            System.out.println(i);
+        }
+    }
+}
